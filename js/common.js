8e88400122c7c3f270bbcf19a6b67ed92c4c7808
김성은 <kse012050@naver.com>
2022-10-11 14:27:37 +0900

    PC 퍼블리싱 완료

--- js/common.js
+++ js/common.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
     var swiper = new Swiper(".mySwiper", {
         slidesPerView: 3,
-        spaceBetween: 30,
+        spaceBetween: 20,
     });
+
+    $('.FAQArea ul li button').click(function(){
+        $(this).toggleClass('active');
+        $(this).next().slideToggle();
+    })
 })
