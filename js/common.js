d00c66d9469f6e5a21f228cb5a593b72c98f0719
김성은 <kse012050@naver.com>
2022-10-14 01:16:38 +0900

    모바일 작업전 백업

--- js/common.js
+++ js/common.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
-    var swiper = new Swiper(".mySwiper", {
+    $(window).scroll(function(){
+        $(window).scrollTop() > 0 ? $('header').addClass('active') : $('header').removeClass('active');
+    })
+
+    // 라인업 슬라이더
+    lineUpSlider();
+
+    // FAQ 클릭 이벤트
+    FAQClick();
+
+    // 모바일 메뉴 클릭
+    mobileMenu();
+})
+
+function lineUpSlider(){
+    var lineUpSlider = new Swiper(".lineUpSlider", {
         breakpoints: {
             320: {
                 slidesPerView: 1,
@@ -11,13 +26,22 @@ $(document).ready(function(){
             },
         }
     });
+}
 
+function FAQClick(){
     $('.FAQArea ul li button').click(function(){
         $(this).toggleClass('active');
         $(this).next().slideToggle();
     })
+}
 
-    $(window).scroll(function(){
-        $(window).scrollTop() > 0 ? $('header').addClass('active') : $('header').removeClass('active');
+function mobileMenu(){
+    $('header div button').click(function(){
+        $('header div nav').fadeToggle().css('display' , 'flex');
+        $(this).toggleClass('active');
+        $('header').addClass('active');
+        if(!$(this).hasClass('active') && $(window).scrollTop() <= 0){
+            $('header').removeClass('active');
+        }
     })
-})
+}
